d907bc0a277b29e881f0451b2e6f19887262a9c3
XXAmethystXX <amethyst.xjp@gmail.com>
2015-11-21 22:17:22 +0100

    Added code for fixing delay errors.

--- src/autoResynch/AutoResynch.java
+++ src/autoResynch/AutoResynch.java
@@ -7,12 +7,13 @@ public class AutoResynch {
 	
 	static int sampling = 16;
 	static int windowSize = sampling*750;
+	static int threshold = 4000;
 	
 	public static void main(String[] args) throws IOException {
 				
 		System.out.print("Loading subtitle file...");
-		SRTParser srt1 = new SRTParser("test/Greys.Anatomy.s12e05.italiansubs.srt", sampling);
-		
+//		SRTParser srt1 = new SRTParser("test/Greys.Anatomy.s12e05.italiansubs.srt", sampling);
+		SRTParser srt1 = new SRTParser("test/sample.srt", sampling);
 		SRTParser srt2 = new SRTParser();
 		
 		srt2 = (SRTParser) DeepCopy.copy(srt1);
@@ -35,6 +36,7 @@ public class AutoResynch {
 		int[] currentSampleSet;
 		int delayedPeak = 0;
 		int delay = 0;
+		int[] delays = new int[nLines];
 		
 		double avg1 = w1.getAverageAmplitude();
 		double avg2 = w2.getAverageAmplitude();
@@ -51,19 +53,44 @@ public class AutoResynch {
 			delayedPeak = w2.compareNSamplesWithAllSamplesBound(currentSampleSet, srt2.getPeak(i), 48000 + delay/2);//, w1.getAverageAmplitudeNumber(i));
 				
 			delay = delayedPeak - srt2.getPeak(i);
-			System.out.println(delay);
-			srt2.applyDelayFrom(i, delay);
+			System.out.println(delay);						
+			delays[i] = delay;
 			
+			srt2.applyDelayFrom(i, delay);
 			//srt2.print(i);
 
 		}
 		
+		System.out.println("Search delays for errors...");
 		
+		int countErrors = 0;
+		int sumErrors = 0;
+
+		for (int j = 0; j < delays.length; j++){
+			if(Math.abs(delays[j]) > threshold) {
+				countErrors++;
+				
+				if(countErrors > 1) {
+					sumErrors += delays[j-1];
+				}
+			}
+			else {
+				if(countErrors > 1) {
+					sumErrors += delays[j-1];
+					
+					for(int k = 0; k < countErrors; k++) {
+						delays[j-k-1] = Math.round(sumErrors/countErrors);
+					}
+				}				
+				sumErrors = 0;
+				countErrors = 0;
+			}
+			 
+		}
+		
+		for(int d : delays) {
+			System.out.println(d);	
+		}				
 	}
-	
-
-	
-
-
 }
 
